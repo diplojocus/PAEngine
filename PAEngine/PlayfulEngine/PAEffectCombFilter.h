7a7bbedb40f261c2e8e8e629844697a7e63f3f08
Joe White <white.joe4@gmail.com>
2013-04-11 10:53:53 +0100

    added CombFilter class

---
+++ PAEngine/PlayfulEngine/PAEffectCombFilter.h
@@ -0,0 +1,13 @@
+//
+//  PAEffectCombFilter.h
+//  PAEngine
+//
+//  Created by Joe White on 10/04/2013.
+//  Copyright (c) 2013 Joe White. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PAEffectCombFilter : NSObject
+
+@end
---
+++ PAEngine/PlayfulEngine/PAEffectCombFilter.m
@@ -0,0 +1,13 @@
+//
+//  PAEffectCombFilter.m
+//  PAEngine
+//
+//  Created by Joe White on 10/04/2013.
+//  Copyright (c) 2013 Joe White. All rights reserved.
+//
+
+#import "PAEffectCombFilter.h"
+
+@implementation PAEffectCombFilter
+
+@end
